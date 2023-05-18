9eeea3032996053353e705b1ee3e8ea552f3726b
hypersu <623661487@qq.com>
2023-05-18 17:52:10 +0800

    文件操作修改

--- file_operations.c
+++ file_operations.c
@@ -3,7 +3,6 @@
 //
 
 #include <stdlib.h>
-#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -11,6 +10,7 @@
 
 void open_file() {
     int fd;
+    // 打开文件描述符
     fd = open("C:\\Users\\suxiaohan\\Desktop\\1.txt", O_RDWR, S_IRUSR | S_IWUSR | S_IXUSR);
 
     printf("文件描述符：%d", fd);
@@ -18,9 +18,15 @@ void open_file() {
     // 分配内存
     char *p = malloc(BUFF_SIZE);
 
+    // 读取文件
     read(fd, p, sizeof(p));
 
     printf("文件中的内容：%s", p);
 
+    // 释放内存
     free(p);
+
+    // 关闭文件描述符
+    close(fd);
+
 }
